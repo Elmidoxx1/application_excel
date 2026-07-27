4e1d36dd7ed378fcbea0765a55616fb01c4300cd
Elmidoxx1 <125566486+Elmidoxx1@users.noreply.github.com>
2026-07-27 16:07:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Application démarrée");
+const fichierExcel = "قائمة توزيع سفيان.xlsx";
+
+fetch(fichierExcel)
+.then(response => response.arrayBuffer())
+.then(data => {
+    const workbook = XLSX.read(data, {
+        type: "array"
+    });
+
+    const feuille = workbook.Sheets[workbook.SheetNames[0]];
+
+    const lignes = XLSX.utils.sheet_to_json(feuille);
+
+    const tableau = document.getElementById("tableau");
+
+    lignes.forEach(ligne => {
+
+        let tr = document.createElement("tr");
+
+        tr.innerHTML = `
+        <td>${ligne.Nom || ""}</td>
+        <td>${ligne.Montant || ""}</td>
+        <td>${ligne.Service || ""}</td>
+        `;
+
+        tableau.appendChild(tr);
+    });
+});
